5235285185f4aa23914c9cd1e2a9a04ca327a88e
Yunhao Qian <yunhaoqian@hotmail.com>
2025-09-14 13:42:15 -0400

    Add .clang-format and finish part 1

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -1,13 +1,12 @@
-#include <cstdio>
 #include "cpu.h"
+#include <cstdio>
 
 #include "common.h"
 
 namespace StreamCompaction {
     namespace CPU {
         using StreamCompaction::Common::PerformanceTimer;
-        PerformanceTimer& timer()
-        {
+        PerformanceTimer &timer() {
             static PerformanceTimer timer;
             return timer;
         }
@@ -15,11 +14,16 @@ namespace StreamCompaction {
         /**
          * CPU scan (prefix sum).
          * For performance analysis, this is supposed to be a simple for loop.
-         * (Optional) For better understanding before starting moving to GPU, you can simulate your GPU scan in this function first.
+         * (Optional) For better understanding before starting moving to GPU, you can simulate your
+         * GPU scan in this function first.
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            int sum = 0;
+            for (int i = 0; i < n; ++i) {
+                odata[i] = sum;
+                sum += idata[i];
+            }
             timer().endCpuTimer();
         }
 
@@ -30,9 +34,15 @@ namespace StreamCompaction {
          */
         int compactWithoutScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            int o_count = 0;
+            for (int i = 0; i < n; ++i) {
+                if (idata[i] != 0) {
+                    odata[o_count] = idata[i];
+                    ++o_count;
+                }
+            }
             timer().endCpuTimer();
-            return -1;
+            return o_count;
         }
 
         /**
@@ -42,9 +52,24 @@ namespace StreamCompaction {
          */
         int compactWithScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            // Step 1: map
+            int *bools = new int[n];
+            for (int i = 0; i < n; ++i) {
+                bools[i] = (idata[i] != 0) ? 1 : 0;
+            }
+            // Step 2: scan
+            int *indices = new int[n];
+            scan(n, indices, bools);
+            // Step 3: scatter
+            int o_count = 0;
+            for (int i = 0; i < n; ++i) {
+                if (bools[i] == 1) {
+                    odata[indices[i]] = idata[i];
+                    ++o_count;
+                }
+            }
             timer().endCpuTimer();
-            return -1;
+            return o_count;
         }
     }
 }
