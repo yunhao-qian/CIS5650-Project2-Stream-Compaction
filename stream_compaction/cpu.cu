04a994024895771b339c5565f5b98c1040e21b29
Yunhao Qian <yunhaoqian@hotmail.com>
2025-09-14 14:48:03 -0400

    Finish part 2

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -1,7 +1,7 @@
-#include "cpu.h"
 #include <cstdio>
 
 #include "common.h"
+#include "cpu.h"
 
 namespace StreamCompaction {
     namespace CPU {
@@ -34,15 +34,15 @@ namespace StreamCompaction {
          */
         int compactWithoutScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            int o_count = 0;
+            int oCount = 0;
             for (int i = 0; i < n; ++i) {
                 if (idata[i] != 0) {
-                    odata[o_count] = idata[i];
-                    ++o_count;
+                    odata[oCount] = idata[i];
+                    ++oCount;
                 }
             }
             timer().endCpuTimer();
-            return o_count;
+            return oCount;
         }
 
         /**
@@ -51,25 +51,27 @@ namespace StreamCompaction {
          * @returns the number of elements remaining after compaction.
          */
         int compactWithScan(int n, int *odata, const int *idata) {
+            int *bools = new int[n];
+            int *indices = new int[n];
             timer().startCpuTimer();
             // Step 1: map
-            int *bools = new int[n];
             for (int i = 0; i < n; ++i) {
                 bools[i] = (idata[i] != 0) ? 1 : 0;
             }
             // Step 2: scan
-            int *indices = new int[n];
             scan(n, indices, bools);
             // Step 3: scatter
-            int o_count = 0;
+            int oCount = 0;
             for (int i = 0; i < n; ++i) {
                 if (bools[i] == 1) {
                     odata[indices[i]] = idata[i];
-                    ++o_count;
+                    ++oCount;
                 }
             }
             timer().endCpuTimer();
-            return o_count;
+            delete[] bools;
+            delete[] indices;
+            return oCount;
         }
     }
 }
--- stream_compaction/naive.cu
+++ stream_compaction/naive.cu
@@ -1,25 +1,59 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
+
 #include "common.h"
 #include "naive.h"
 
 namespace StreamCompaction {
     namespace Naive {
         using StreamCompaction::Common::PerformanceTimer;
-        PerformanceTimer& timer()
-        {
+        PerformanceTimer &timer() {
             static PerformanceTimer timer;
             return timer;
         }
-        // TODO: __global__
+
+        __global__ void kernScan(int n, int offset, int *odata, const int *idata) {
+            int index = blockIdx.x * blockDim.x + threadIdx.x;
+            if (index >= n) {
+                return;
+            }
+            int beforeIndex = index - offset;
+            odata[index] = idata[index] + (beforeIndex < 0 ? 0 : idata[beforeIndex]);
+        }
+
+        __global__ void kernShiftRight(int n, int *odata, const int *idata) {
+            int index = blockIdx.x * blockDim.x + threadIdx.x;
+            if (index >= n) {
+                return;
+            }
+            odata[index] = index == 0 ? 0 : idata[index - 1];
+        }
 
         /**
          * Performs prefix-sum (aka scan) on idata, storing the result into odata.
          */
         void scan(int n, int *odata, const int *idata) {
+            const auto dataSize = n * sizeof(int);
+
+            int *dev_iData;
+            cudaMalloc((void **)&dev_iData, dataSize);
+            cudaMemcpy(dev_iData, idata, dataSize, cudaMemcpyHostToDevice);
+            int *dev_oData;
+            cudaMalloc((void **)&dev_oData, dataSize);
+
             timer().startGpuTimer();
-            // TODO
+            const int blockSize = 256;
+            const int gridSize = (n + blockSize - 1) / blockSize;
+            for (int offset = 1; offset < n; offset *= 2) {
+                kernScan<<<gridSize, blockSize>>>(n, offset, dev_oData, dev_iData);
+                std::swap(dev_oData, dev_iData);
+            }
+            kernShiftRight<<<gridSize, blockSize>>>(n, dev_oData, dev_iData);
             timer().endGpuTimer();
+
+            cudaMemcpy(odata, dev_oData, dataSize, cudaMemcpyDeviceToHost);
+            cudaFree(dev_iData);
+            cudaFree(dev_oData);
         }
     }
 }
